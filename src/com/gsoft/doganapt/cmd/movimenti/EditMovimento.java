48a2f14864f7acc514fc3ff19c0397034aeb0008
gtron <gtron@550366ce-cb87-43f9-b4b8-46b539e9bbca>
2011-08-15 12:51:28 +0000

    git-svn-id: svn+ssh://www.gianlucamereu.it/mnt/svn/DoganaPV@134 550366ce-cb87-43f9-b4b8-46b539e9bbca

--- src/com/gsoft/doganapt/cmd/movimenti/EditMovimento.java
+++ src/com/gsoft/doganapt/cmd/movimenti/EditMovimento.java
@@ -24,59 +24,65 @@ import com.gtsoft.utils.http.servlet.GtServlet;
 
 
 public class EditMovimento extends BeanEditor {
-	
+
 	protected static String TEMPLATE = "movimenti/" + DEFAULT_TEMPLATE;
-	
+
 	String template ;
 	boolean isIva = false ;
-	
-	public EditMovimento ( GtServlet callerServlet) {
+
+	public EditMovimento ( final GtServlet callerServlet) {
 		super(callerServlet);
 		template = TEMPLATE;
 	}
-	public Template exec(HttpServletRequest req, HttpServletResponse resp, Context ctx) throws Exception  {
-		
+	@Override
+	public Template exec(final HttpServletRequest req, final HttpServletResponse resp, final Context ctx) throws Exception  {
+
 		isIva =  "1".equals( getParam("iva", false) ) ;
-		
+
 		ctx.put("isIva", isIva) ;
-		
-		Collection<?> stalli = StalloAdapter.getAllCached();
+
+		final Collection<?> stalli = StalloAdapter.getAllCached();
 		ctx.put("merci" ,  MerceAdapter.getAllCached());
 		ctx.put("iters" ,  IterAdapter.getAllCached());
 		ctx.put("stalli" ,  stalli);
-		
-		Template t = super.exec(req, resp, ctx) ; 
-		
-		Movimento m = (Movimento) ctx.get(ContextKeys.OBJECT);
-		
-		
+
+		final Template t = super.exec(req, resp, ctx) ;
+
+		final Movimento m = (Movimento) ctx.get(ContextKeys.OBJECT);
+
+		m.getConsegna().flushRegistri();
+
 		if ( getBooleanParam(Strings.EXEC) ) {
 
-			HashMap<String, String[]> newParams = new HashMap<String, String[]>(1);
-			String[] id = {m.getIdConsegna().toString()} ;
+			final HashMap<String, String[]> newParams = new HashMap<String, String[]>(1);
+			final String[] id = {m.getIdConsegna().toString()} ;
 			newParams.put( "id", id );
 			setNextCommand(
 					callerServlet.getCommand(ConsegneServlet.Commands.DEFAULT, request, response) , newParams);
 		}
-		
+
 		return t;
 	}
 
 
+	@Override
 	public VelocityCommand clone() {
-		return  new EditMovimento(this.callerServlet);
+		return  new EditMovimento(callerServlet);
 	}
 	MovimentoAdapter adapter = null ;
+	@Override
 	public BeanAdapter2 getAdapter(  )  {
 		if ( adapter == null ) {
-			if ( isIva )
+			if ( isIva ) {
 				adapter = new MovimentoIvaAdapter();
-			else 
+			} else {
 				adapter = new MovimentoDoganaleAdapter();
+			}
 		}
-		
+
 		return adapter;
 	}
+	@Override
 	public String getTemplateName() {
 		return template;
 	}
