9c72f64bdf71201459772e4943da9ea852941572
gtron <gtron@550366ce-cb87-43f9-b4b8-46b539e9bbca>
2012-03-05 00:29:03 +0000

    Piccolo refactoring
    git-svn-id: svn+ssh://www.gianlucamereu.it/mnt/svn/DoganaPV@160 550366ce-cb87-43f9-b4b8-46b539e9bbca

--- src/com/gsoft/doganapt/cmd/consegne/ApriConsegna.java
+++ src/com/gsoft/doganapt/cmd/consegne/ApriConsegna.java
@@ -1,7 +1,7 @@
 package com.gsoft.doganapt.cmd.consegne;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,101 +24,118 @@ import com.gtsoft.utils.http.servlet.GtServlet;
 
 
 public class ApriConsegna extends VelocityCommand {
-	
+
 	protected static String TEMPLATE = "consegne/list.vm" ;
-	
+
+	StalloAdapter sAdp = null ;
+
 	public ApriConsegna ( GtServlet callerServlet) {
 		super(callerServlet);
 	}
+	@Override
 	public Template exec(HttpServletRequest req, HttpServletResponse resp, Context ctx) throws Exception  {
-		
+
 		Integer id = getIntParam("id", true);
-		
+
 		Consegna c = ConsegnaAdapter.get(id);
-		
+
 		String note = getParam("note", false);
-		
+
 		FormattedDate data = getDateParam("data", true);
 		String doc = getParam("doc", false);
 		String doc_num = getParam("doc_num", false);
 		FormattedDate doc_data = getDateParam("doc_data", false);
-		
-		Documento documento = Documento.getDocumento( doc, doc_data , doc_num ); 
-		
+
+		Documento documento = Documento.getDocumento( doc, doc_data , doc_num );
+
 		String docPV = getParam("docPV", false);
 		String docPV_num = getParam("docPV_num", false);
 		FormattedDate docPV_data = getDateParam("docPV_data", false);
-		
-		Documento documentoPV = Documento.getDocumento( docPV, docPV_data , docPV_num ); 
-		
+
+		Documento documentoPV = Documento.getDocumento( docPV, docPV_data , docPV_num );
+
+
+
 		ctx.put( ContextKeys.OBJECT , c ) ;
-		
+
 		if ( c != null && getBooleanParam(Strings.EXEC) ) {
-			
+
 			MovimentoQuadrelliAdapter qAdp = new MovimentoQuadrelliAdapter(PtMovimentazioniImporter.getInstance().getAccessDB());
-			
+
 			ArrayList<String> stalli = qAdp.getCodiciStalli(c, data);
 			Stallo s ;
-			StalloAdapter sAdp = Stallo.newAdapter() ;
-			
-			for ( Iterator<String> i = stalli.iterator() ; i.hasNext() ; ){
-				s = (Stallo) sAdp.getByCodice( i.next() ) ;
-				
+			sAdp = Stallo.newAdapter() ;
+
+			for (String string : stalli) {
+				s = (Stallo) sAdp.getByCodice( string ) ;
+
 				if ( s != null ) {
-//					if ( s.getIdConsegnaAttuale() == null) {
-						s.setIdConsegnaAttuale(id);
-						s.setIdConsegnaPrenotata(null);
-						s.setImmessoInLiberaPratica(Boolean.FALSE) ;
-						
-						s.setAttuale( new Double(0) );
-						s.setCaricato( new Double(0) );
-						
-						sAdp.update(s);
-//					}
-//					else if ( ! c.getIdIter().equals( 4 )) {
-//						
-//						ctx.put( "list" ,  new ConsegnaAdapter().getNonChiuse( ) ) ;
-//						throw new UserException("Attenzione, lo stallo " + s  + " non è libero !") ;
-//					}
+					if ( s.getIdConsegnaAttuale() == null) {
+						assegnaStallo(s, id);
+					}
+					else {
+						Consegna attuale = ConsegnaAdapter.get(s.getIdConsegnaAttuale());
+
+						if ( attuale == null || attuale.isChiusa() ) {
+							assegnaStallo(s, id);
+						} else {
+							/*
+							ctx.put( "list" ,  new ConsegnaAdapter().getNonChiuse( ) ) ;
+							throw new UserException("Attenzione, lo stallo " + s  + " non è libero !") ;
+							 */
+						}
+					}
 				}
 			}
-			
+
 			if ( c.isChiusa() ) {
-//				c.setDataChiusura(null);
-//				Consegna.newAdapter().update(c);
+				//				c.setDataChiusura(null);
+				//				Consegna.newAdapter().update(c);
 			}
 			else {
 				c.getIter()
-					.getImporter(
+				.getImporter(
 						new MovimentoDoganaleAdapter(),
-						new MovimentoIvaAdapter(), 
+						new MovimentoIvaAdapter(),
 						qAdp
 						)
-						
+
 						.apriConsegna(c, data, documento, documentoPV, note);
 			}
-			
-			
-			
-			
+
+
+
+
 			c.setDataChiusura(null);
 			Consegna.newAdapter().update(c);
-			
+
 			ctx.put( "list" ,  new ConsegnaAdapter().getNonChiuse( ) ) ;
-			
+
 			ctx.put("result", Boolean.TRUE ) ;
-			
+
 			response.sendRedirect(".consegne?id=" + id );
 
 		}
 		return null ;
 	}
-	
 
-	public VelocityCommand clone() {
-		return  new ApriConsegna(this.callerServlet);
+	protected void assegnaStallo(Stallo s, Integer idConsegna ) throws IOException {
+		s.setIdConsegnaAttuale(idConsegna);
+		s.setIdConsegnaPrenotata(null);
+		s.setImmessoInLiberaPratica(Boolean.FALSE) ;
+
+		s.setAttuale( new Double(0) );
+		s.setCaricato( new Double(0) );
+
+		sAdp.update(s);
 	}
-	
+
+	@Override
+	public VelocityCommand clone() {
+		return  new ApriConsegna(callerServlet);
+	}
+
+	@Override
 	public String getTemplateName() {
 		return TEMPLATE;
 	}
