0a6636491caa8318b94ddf1b9c1de803859c2aaa
gtron <gtron@550366ce-cb87-43f9-b4b8-46b539e9bbca>
2012-05-14 22:19:36 +0000

    git-svn-id: svn+ssh://www.gianlucamereu.it/mnt/svn/DoganaPV@184 550366ce-cb87-43f9-b4b8-46b539e9bbca

--- src/com/gsoft/doganapt/cmd/Login.java
+++ src/com/gsoft/doganapt/cmd/Login.java
@@ -13,23 +13,25 @@ import com.gtsoft.utils.http.servlet.GtServlet;
 public class Login extends VelocityCommand {
 
 	protected static final String T = "login.vm" ;
-	
+
 	public Login ( GtServlet callerServlet) {
 		super(callerServlet);
 	}
 
+	@Override
 	public VelocityCommand clone() {
-		return  new Login(this.callerServlet);
+		return  new Login(callerServlet);
 	}
 
+	@Override
 	public Template exec(HttpServletRequest req, HttpServletResponse resp, Context ctx) throws Exception  {
-		
+
 		HttpSession s =  req.getSession();
-		
+
 		String user = getParam("user", false);
 		String pwd  = getParam("pwd", false);
 		if ( "user".equals(user)) {
-			
+
 			if ( pwd != null && pwd.equals("d0gan4") ) {
 				s.setAttribute("logged", Boolean.TRUE) ;
 				s.setAttribute("admin", Boolean.FALSE) ;
@@ -40,7 +42,7 @@ public class Login extends VelocityCommand {
 			}
 		}
 		else if ( "admin".equals(user)) {
-			
+
 			if ( pwd != null && pwd.equals("s1lente") ) {
 				s.setAttribute("logged", Boolean.TRUE) ;
 				s.setAttribute("admin", Boolean.TRUE) ;
@@ -53,26 +55,17 @@ public class Login extends VelocityCommand {
 		else {
 			s.setAttribute("logged", Boolean.FALSE) ;
 		}
-		
+
 		ctx.put("user", user) ;
-		
-//		BeanAdapter2 adp = BeanAdapter2.newAdapter();
-//		ctx.put( ContextKeys.LIST , adp.getAll() ) ;
-//		
-//		Integer selectedId = getParameter.getInt(ID, req, false);
-//		
-//		if ( selectedId != null ) {
-//			ctx.put( ContextKeys.OBJECT , adp.getByKey(selectedId) ) ;
-//		}
+
+		Homepage.purgeCaches();
+
 		return null;
 	}
 
-	
+	@Override
 	public String getTemplateName() {
 		return T ;
 	}
-	
-	
 
-	
 }
